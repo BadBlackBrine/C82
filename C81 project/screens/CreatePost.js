809453e03034780fccf4c637f9a0c7002c5b04d4
꧁༒☬Ðєαтн☬༒꧂ <88191864+BadBlackBrine@users.noreply.github.com>
2022-08-15 19:46:06 +0530

    Add files via upload

---
+++ C81 project/screens/CreatePost.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { findNodeHandle, Text, View } from 'react-native';
+
+export default class CreatePost extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>simple create post text</Text>
+            </View>
+        )
+            }
+        }
+
+
+        
